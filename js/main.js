94161d55f330023796da0b709e007496adf49e5d
Alex <alex@Matthews-MacBook-Pro.local>
2017-07-25 22:27:34 -0400

    Version 72517

--- js/main.js
+++ js/main.js
@@ -3,21 +3,47 @@ $(document).ready(function() {
   $('#mobile-menu').hide();
 
   $('#left-info-slider').hover(function() {
-    $('#left-info').toggle(600);
+    $('#left-info').slideToggle(600);
   });
 
   $('#right-info-slider').hover(function() {
-    $('#right-info').toggle(600);
+    $('#right-info').slideToggle(600);
   });
 
-    $('.lnr-menu').click( function(event){
+    $('#menu').click( function(event){
          event.stopPropagation();
-         $('#mobile-menu').toggle();
+         $('#mobile-menu').slideToggle(600);
+         $('#menu').css('visibility','hidden');
      });
 
      $(document).click( function(){
          $('#mobile-menu').hide();
+         $('#menu').css('visibility','visible');
      });
 
+     $('#profile1').hover(function() {
+       $('#profile1-learn-more').toggle();
+     });
+
+     $('#profile2').hover(function() {
+       $('#profile2-learn-more').toggle();
+     });
+
+     $('#profile3').hover(function() {
+       $('#profile3-learn-more').toggle();
+     });
+
+     var ias = $.ias({
+       container:  ".blog-container",
+       item:       ".post",
+       pagination: "#pagination",
+       next:       ".next"
+     });
+
+    ias.extension(new IASSpinnerExtension());            // shows a spinner (a.k.a. loader)
+    ias.extension(new IASTriggerExtension({offset: 2})); // shows a trigger after page 3
+    ias.extension(new IASNoneLeftExtension({
+     text: 'There are no more pages left to load.'      // override text when no pages left
+    }));
 
 })
