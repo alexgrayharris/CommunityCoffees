ca49f14f5c88f337b4b16b3b9b1fac7dcd32eda6
Alex <alex@Matthews-MacBook-Pro.local>
2017-07-30 23:03:54 -0400

    73017

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
+  // Hide the mobile menu unless click on menu icon
   $('#mobile-menu').hide();
 
+  // Show description when you hover over advocate info
   $('#left-info-slider').hover(function() {
     $('#left-info').slideToggle(600);
   });
@@ -10,31 +12,33 @@ $(document).ready(function() {
     $('#right-info').slideToggle(600);
   });
 
-    $('#menu').click( function(event){
-         event.stopPropagation();
-         $('#mobile-menu').slideToggle(600);
-         $('#menu').css('visibility','hidden');
-     });
+  // When you click on mobile menu icon show mobile menu
+  $('#menu').click( function(event){
+     event.stopPropagation();
+     $('#mobile-menu').slideToggle(600);
+     $('#menu').css('visibility','hidden');
+   });
+  // When you click anywhere in the document close mobile menu if open
+  $(document).click( function(){
+     $('#mobile-menu').hide();
+     $('#menu').css('visibility','visible');
+   });
 
-     $(document).click( function(){
-         $('#mobile-menu').hide();
-         $('#menu').css('visibility','visible');
-     });
+  // When you hover over community profiles show button
+  $('#profile1').hover(function() {
+     $('#profile1-learn-more').toggleClass('show');
+   });
 
-     $('#profile1').hover(function() {
-       $('#profile1-learn-more').toggleClass('show');
+  $('#profile2').hover(function() {
+     $('#profile2-learn-more').toggleClass('show');
+  });
 
-     });
-
-     $('#profile2').hover(function() {
-       $('#profile2-learn-more').toggleClass('show');
-     });
-
-     $('#profile3').hover(function() {
+  $('#profile3').hover(function() {
        $('#profile3-learn-more').toggleClass('show');
-     });
+   });
 
-     var ias = $.ias({
+  // Plugin for lazy scrolling on blog
+  var ias = $.ias({
        container:  ".blog-container",
        item:       ".post",
        pagination: "#pagination",
